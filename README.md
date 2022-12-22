ba6f93810de688c8efeab3d18026b15290cd3337
malko <jgotti@jgotti.org>
2022-12-22 18:39:16 +0100

    Add licence and readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# Battery-indicator (upower)
+This is a gnome-shell extension to display your connected devices battery informations as reported by upower in the status panel.
+
+Clicking on the indicators will reveal a menu which will let you display more informations about connected devices:
+* icon identifier
+* icon of battery state as reported by upower
+* device model
+* (state of the battery)
+* battery level percentage
+
+Good to know:
+upower is not always reliable on reporting battery level for connected devices. Generally it will tell you that you should ignore the reported battery level. In such cases this extensions will write the percentage level indicato in italic. Same representation will be used for corresponding popup menu entry.
+
+
+# Settings
+You can via the extension prefs set the refresh interval and wether to add a manual refresh button or not. By default the refresh interval is 5 minutes.
+
+Also you can decide to show/hide devices from the panel indicator by clicking them in the indicator menu. item with now check mark in front of it are marked as hidden.
+
+
+# Screenshots
+
+## status indicators and menu
+![image](./screenshots/full.png)
+
+## status indicators
+![image](./screenshots/indicators.png)
+
+## indicator popup menu
+![image](./screenshots/menu.png)
+
+## settings
+![image](./screenshots/settings.png)
+
--- src/prefs.js
+++ src/prefs.js
@@ -1,3 +1,20 @@
+/* pref.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 const {Adw, GLib, Gtk, Gio} = imports.gi
 
 // It's common practice to keep GNOME API and JS imports in separate blocks
@@ -11,35 +28,8 @@ const Me = ExtensionUtils.getCurrentExtension()
  * @param {ExtensionMeta} meta - An extension meta object, described below.
  */
 function init(meta) {
-	// log(`initializing ${meta.metadata.name} Preferences`)
+
 }
-
-/**
- * This function is called when the preferences window is first created to build
- * and return a GTK widget.
- *
- * As of GNOME 42, the preferences window will be a `Adw.PreferencesWindow`.
- * Intermediate `Adw.PreferencesPage` and `Adw.PreferencesGroup` widgets will be
- * used to wrap the returned widget if necessary.
- *
- * @returns {Gtk.Widget} the preferences widget
- */
-// function buildPrefsWidget() {
-// 	// This could be any GtkWidget subclass, although usually you would choose
-// 	// something like a GtkGrid, GtkBox or GtkNotebook
-// 	const prefsWidget = new Gtk.Label({
-// 		label: Me.metadata.name,
-// 		visible: true,
-// 	})
-
-// 	// Add a widget to the group. This could be any GtkWidget subclass,
-// 	// although usually you would choose preferences rows such as AdwActionRow,
-// 	// AdwComboRow or AdwRevealerRow.
-// 	const label = new Gtk.Label({ label: `${Me.metadata.name}` })
-// 	group.add(label)
-
-// 	window.add(page)
-// }
 
 /**
  * This function is called when the preferences window is first created to fill
